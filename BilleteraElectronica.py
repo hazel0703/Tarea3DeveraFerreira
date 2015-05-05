acea68e3a666cf5d6dd285d39547e692e7020593
Adriana Devera <apdr83@gmail.com>
2015-05-05 13:26:47 -0430

    Se creó el método recargar, y a la clase se le agregó el parámetro créditos, realizado por Adriana Devera

--- BilleteraElectronica.py
+++ BilleteraElectronica.py
@@ -6,12 +6,16 @@ Created on 5/5/2015
 '''
 
 class BilleteraElectronica:
-    def __init__(self, ID, Nombres, Apellidos, CI, PIN):
+    def __init__(self, ID, Nombres, Apellidos, CI, PIN, creditos = []):
         self.ID = ID
         self.Nombres = Nombres
         self.Apellidos = Apellidos
         self.CI = CI
         self.PIN = PIN
+        self.creditos = creditos
+        
+    def recargar(self,monto,fecha,idEstablecimiento):
+        self.creditos.append((monto,fecha,idEstablecimiento))
 
 if __name__ == '__main__':
     pass
--- test_BilleteraElectronica.py
+++ test_BilleteraElectronica.py
@@ -4,14 +4,22 @@ Created on 5/5/2015
 @author: Usuario
 '''
 import unittest
-from BilleteraElectronica import * 
+from BilleteraElectronica import *
+from datetime import datetime 
 
 
 class Test(unittest.TestCase):
 
-
+    #Caso para comprobar que la clase Billetera electronica existe
     def testClassBE(self):
         BilleteraElectronica(5,"Antonio", "Perez", 12345678, 4321)
+        
+    
+    #Caso para probar que existe el metodo recargar
+    def testMetodRecargar(self):
+        BE = BilleteraElectronica(5,"Antonio", "Perez", 12345678, 4321)
+        Fecha_recarga = datetime(2015, 4, 21, 6, 15)
+        BE.recargar(100, Fecha_recarga, id)
 
 
 if __name__ == "__main__":
