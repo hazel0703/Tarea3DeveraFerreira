cc2841e88f1fd5e95245d1bebb0ffda617fa249c
Carlos <cferreira6594@gmail.com>
2015-05-05 16:19:48 -0430

    Se agrego el metodo consumir y el paramatro debitos, se acomodo la inicializacion del monto del constructor. Carlos

--- BilleteraElectronica.py
+++ BilleteraElectronica.py
@@ -6,14 +6,16 @@ Created on 5/5/2015
 '''
 
 class BilleteraElectronica:
-    def __init__(self, ID, Nombres, Apellidos, CI, PIN, creditos = [], monto = 0):
+    def __init__(self, ID, Nombres, Apellidos, CI, PIN, creditos = [], monto = 0, debitos = []):
         self.ID = ID
         self.Nombres = Nombres
         self.Apellidos = Apellidos
         self.CI = CI
         self.PIN = PIN
         self.creditos = creditos
-        self.monto = 0
+        self.monto = monto
+        self.debitos = debitos
+        
         
     def recargar(self,monto,fecha,idEstablecimiento):
         self.monto = self.monto + monto
@@ -21,6 +23,9 @@ class BilleteraElectronica:
         
     def saldo(self):
         return self.monto
+    
+    def consumir(self, monto, fecha, idEstablecimiento):
+        self.debitos.append((monto, fecha, idEstablecimiento))
 
 if __name__ == '__main__':
     pass
--- test_BilleteraElectronica.py
+++ test_BilleteraElectronica.py
@@ -26,12 +26,18 @@ class Test(unittest.TestCase):
         BE = BilleteraElectronica(5,"Antonio", "Perez", 12345678, 4321)
         BE.saldo()
         
-    #Caso para probar si el método recargar realmente recarga el saldo
+    #Caso para probar si el metodo recargar realmente recarga el saldo
     def testRecargarsaldo(self):
         BE = BilleteraElectronica(5,"Antonio", "Perez", 12345678, 4321)
         Fecha_recarga = datetime(2015, 5, 5, 6, 15)
         BE.recargar(100, Fecha_recarga, id)
         self.assertEqual(BE.saldo(), 100, "El saldo no es el correcto")
+        
+    def testMetodconsumir(self):
+        BE = BilleteraElectronica(5,"Antonio", "Perez", 12345678, 4321)
+        Fecha_consumir = datetime(2015, 5, 5, 6, 15)
+        BE.consumir(100, Fecha_consumir, id)
+        
 
 
 if __name__ == "__main__":
